7bfd172cc6b526bbf6841b08a09aff910df5004a
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-06-06 09:49:42 -0300

    Add files via upload

---
+++ 06.06/eita.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+
+
+float a,b,P;
+char resp;
+main(){
+
+resp= 's';
+
+
+while ((resp == 's') || (resp == 'sim') || (resp == 'S') || (resp == 'SIM')){
+
+    printf("Entre com A:\n");
+    scanf("%f",&a);
+
+    printf("Entre com B: \n");
+    scanf("%f",&b);
+
+   P = a-b;
+
+    printf("O valor de P: %f",P);
+    printf("\n\nDeseja continuar S/N?: ");
+
+    scanf("\n%c",&resp);
+}
+}
