7ed1900a3e5b88743bf4b45e04336105faebc0c4
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-06-06 08:56:46 -0300

    Add files via upload

---
+++ tabuada do 6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+
+main(){
+
+ int num,mult,cont;
+
+
+ num=6;
+
+ while(num==6){
+
+    printf("escrever a taduada do 6: ");
+
+     cont=1;
+
+ while(cont<=10){
+
+    mult=num*cont;
+
+    printf("\nEssa e a tabuada do 6: %d.6=%d",cont,mult);
+
+     cont=cont+1;
+
+
+}
+    num=num+1;
+
+
+ }}
