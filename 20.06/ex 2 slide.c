d4f7ac451b69e6220cb4d77972f2d7965ba7bcd6
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-06-20 08:32:18 -0300

    Add files via upload

---
+++ 20.06/ex 2 slide.c
@@ -0,0 +1,27 @@
+#include<conio.h>
+#include<stdio.h>
+main(){
+
+  int cont,soma,num;
+
+  cont=100;
+
+  soma=0;
+
+  do{
+
+   soma=soma+cont;
+
+   cont=cont+2;
+
+  }while(cont<=200);
+
+  printf("\na soma dos numeros pares: %d", soma);
+
+
+
+
+
+}
+
+
