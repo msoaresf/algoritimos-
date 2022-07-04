e5f917ac6070a678f689f573f65b0c52e014af1c
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-04 08:32:01 -0300

    Add files via upload

---
+++ 04.07/slide ex2.c
@@ -0,0 +1,34 @@
+#include <conio.h>
+#include <stdio.h>
+
+main(){
+
+  int cont;
+  float soma,media,n;
+
+
+
+  for(cont=0; cont<100; cont++){
+
+
+  printf("entre com o valor:");
+  scanf("%f",&n);
+
+  soma=soma+n;
+
+  }
+
+  media=soma/cont;
+
+  printf("soma: %f", soma);
+  printf("media: %f",media);
+
+
+
+
+
+
+
+
+
+}
