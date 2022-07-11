69e3a279c4e12699f7c9e2cfc5c42d869ab561da
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-11 08:22:19 -0300

    Add files via upload

---
+++ ex1, lista 2 for.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+
+ float nota,maior, i;
+
+
+ for(i=0; i<=10; i++){
+
+
+    printf("entre com a nota: ");
+    scanf("%f",&nota);
+
+    if(nota>maior){
+
+      maior=nota;
+
+    }
+
+
+
+
+ }
+
+printf("a maior nota e: %f",nota);
+
+
+
+
+
+
+}
