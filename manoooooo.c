f41303504c3aa9a992358b4d31455295cc2aef98
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-11 09:39:04 -0300

    Add files via upload

---
+++ manoooooo.c
@@ -0,0 +1,76 @@
+#include<math.h>
+#include<conio.h>
+#include<stdio.h>
+
+
+main(){
+
+ int candidato,soma, eleitor,x, qa,qb,qc,qd,qe,qf;
+
+
+ for(x=0; x<=2000; x++){
+
+
+
+ printf("digite 1 para o primeiro candidato:\n" );
+
+ printf("digite 2 para para o segundo candidato:\n" );
+
+ printf("digite 3 para o terceiro candidato:\n" );
+
+ printf("digite 4 para o quarto candidato:\n" );
+
+ printf("digite 5 para voto nulo:\n" );
+
+ printf("digite 6 para voto em branco:\n" );
+
+
+     printf("\nentre com seu codigo: ");
+ scanf("%d",&eleitor);
+
+ switch(eleitor){
+ case 1:
+  qa=
+     break;
+
+ case 2:
+  qb=
+    break;
+
+ case 3:
+     qc=
+    break;
+
+ case 4:
+ qd=
+    break;
+ case 5:
+    qe=
+    break;
+
+ case 6:
+    qf=
+    break;
+    }
+    printf("\ntotal de votos candidato 1: %d",qa);
+     printf("\ntotal de votos candidato 2: %d",qb);
+      printf("\ntotal de votos candidato 3: %d",qc);
+       printf("\ntotal de votos candidato 4: %d",qd);
+        printf("\ntotal de votos nulos: %d",qe);
+         printf("\ntotal de votos em branco: %d",qf);
+
+
+
+}
+
+
+
+ }
+
+
+
+
+
+
+
+
