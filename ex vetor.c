a3cb79d34b2ff5e16bd2a2b78ebe51697fddfbad
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-18 08:20:23 -0300

    Add files via upload

---
+++ ex vetor.c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+main(){
+
+ int c,v[10];
+
+ for (c=0;c<10;c++){
+
+    printf("\n entre com a nota: ");
+    scanf("%d",&v[c]);
+
+ }
+
+ for(c=0; c<10; c++){
+
+    printf("\n as notas:%d",v[c]);
+
+ }
+  }
