e3fdc64b04089def74702a83c6f6c175ffd8e2f8
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-18 10:43:39 -0300

    Add files via upload

---
+++ vetor/ex 3 lista.c
@@ -0,0 +1,28 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+main(){
+
+ int c,a[6],soma, multi;
+
+ for (c=0;c<6;c++){
+
+    printf("\n entre com a nota: ");
+    scanf("%d",&a[c]);
+
+     if(c%2==0){
+
+    soma=a[c]+20;
+    printf("elemento B i:%d",soma);
+ }
+ if(c%2==1){
+    a[c]*3==multi;
+
+    printf("\nelemento B ii: %d",multi);
+ }
+
+
+ }
+
+}
---
+++ vetor/numero multiplo de 6.c
@@ -0,0 +1,32 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+main(){
+
+ int v[8],x, m;
+ int c;
+
+ for (c=0;c<8;c++){
+
+    printf("\nEntre com um numero: ");
+    scanf("%d",&v[c]);
+
+
+     if(v[c]/6==0){
+
+        printf("esse e multiplo de 6!!!");
+
+    }
+    }
+
+
+
+ for(c=0;c<8;c++){
+
+    printf ("\nos numeros sao: %d",v[c]);
+ }
+
+
+
+  }
