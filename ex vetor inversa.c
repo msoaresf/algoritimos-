9ffffd85eea3eb265f1626d2fee4fcf0dc348987
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-07-18 08:35:13 -0300

    Add files via upload

---
+++ ex vetor inversa.c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+main(){
+
+ int c,v[6];
+
+ for (c=0;c<6;c++){
+
+    printf("\nEntre com os numeros: ");
+    scanf("%d",&v[c]);
+
+ }
+
+ for(c=5; c>=0; c--){
+
+    printf("\n Os elementos em ordem inversa sao:%d",v[c]);
+
+ }
+  }
