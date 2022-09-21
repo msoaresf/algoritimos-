9fe3fe658ad05d3aeb576c2d58f1baa5db28c70b
msoaresf <103641902+msoaresf@users.noreply.github.com>
2022-09-21 16:58:16 -0300

    Add files via upload

---
+++ orde/ex 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+
+   int i, j=0;
+   char linha[70],linha2[70]; letra;
+
+   printf("Entre com uma frase:\n");
+   gets(linha);
+
+   printf("Agora entre com uma letra:\n");
+   scanf("\n%c",&letra);
+
+   for(i=0; i<70;i++){
+
+    if(linha[i]!=letra){
+        linha2[j]=linha
+
+
+    }
+
+   }
+
+
+
+}
